844565a555026fa1221f7a372448c009c88ce2ed
mizukiwakita <zm18389@josai.ac.jp>
2020-10-20 02:15:50 +0000

    2020.1020

--- ex6-5.js
+++ ex6-5.js
@@ -1,10 +1,12 @@
-function sayhello(){
+function sayhello65(){
     var who ='world';
     alert('Hello,'+ who +'!');
 }
-function taro(){
+function taro65(){
     who ='太郎';
+    alert(who + 'が来た！')
 }
-function hanako(){
+function hanako65(){
     who ='花子';
+    alert(who + 'が来た！')
 }
