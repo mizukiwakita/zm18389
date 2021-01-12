31413b30731c7d255f692ae876aeb71ab06fd3be
mizukiwakita <zm18389@josai.ac.jp>
2021-01-12 02:01:54 +0000

    2021/1/12

--- ex7-4.js
+++ ex7-4.js
@@ -1,6 +1,12 @@
-function dby(){
+function add2(){
     var x = document.getElementById('input3').value;
     var y = document.getElementById('input4').value;
-    alert(Number(x)/Number(y)*Number(y))
+    alert(Number(x)/(Number(y)*Number(y)));
+
+    if(Number(x)/(Number(y)*Number(y)) < 18.5)
+  {alert('低体重')}
+  else if(Number(x)/(Number(y)*Number(y)) < 25 )
+    {alert('普通')}
+  else {alert('肥満')}
 
 }
